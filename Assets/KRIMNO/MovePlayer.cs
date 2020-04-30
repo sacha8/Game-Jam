b3932d81d5f3a3182e7a903073af7731a3a07d01
KRIMNOUILLE <64409680+KRIMNOUILLE@users.noreply.github.com>
2020-04-30 17:49:58 +0200

    Ajoute de mon dossier
    J'ai ajouiter mon dossier + une Scene + un Script que j'ai modifier.

---
+++ Assets/KRIMNO/MovePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    // Quand on lance
+    void Start()
+    {
+        
+    }
+
+    // Chaque secondes
+    void Update()
+    {
+        
+    }
+}
