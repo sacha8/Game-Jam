536233f7692e9751fe4b29b4fda2ad16550ec149
KRIMNOUILLE <64409680+KRIMNOUILLE@users.noreply.github.com>
2020-05-01 22:04:52 +0200

    Ajout du systeme d'inventaire a moitié

---
+++ Assets/krimno/Scripts/Pickup.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    public int Health;
+    public int Potions;
+    public int degats;
+
+    public Text vie;
+
+    private void Start()
+    {
+        
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        Health = 100;
+        vie.text = "Vie : " + Health;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false)
+                {
+                    // ON peut mettre les item dans l'inventaire
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void decreaseHealth()
+    {
+        vie.text = "Vie : " + (Health -= degats); 
+    }
+
+    public void AddHealth()
+    {
+        vie.text = "Vie : " + (Health += Potions);
+    }
+}
