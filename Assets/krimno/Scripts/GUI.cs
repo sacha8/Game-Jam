b46f1e91b84f9f6784f2b038357392acafa03aad
KRIMNOUILLE <64409680+KRIMNOUILLE@users.noreply.github.com>
2020-05-02 16:04:49 +0200

    sacha

---
+++ Assets/krimno/Scripts/GUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GUI : MonoBehaviour
+{
+    public Image craftUi;
+    public GameObject[] j;
+
+    
+
+    private void Update()
+    {
+        CraftUi();
+    }
+
+    public void CraftUi()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            craftUi.enabled = !craftUi.enabled;
+        }
+    }
+    public void ButtonUI()
+    {
+        craftUi.enabled = !craftUi.enabled;
+    }
+
+    
+}
