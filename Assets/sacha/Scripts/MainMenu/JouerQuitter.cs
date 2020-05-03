c14b45b095c92f459de16b7d04f6d9bd109fd982
sacha8 <62705893+sacha8@users.noreply.github.com>
2020-05-03 10:15:21 +0200

    Upload

---
+++ Assets/sacha/Scripts/MainMenu/JouerQuitter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JouerQuitter : MonoBehaviour
+{
+    public string Jouer;
+
+    public void jouer()
+    {
+        SceneManager.LoadScene(Jouer);
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}
