f7dcefe380852d1e4afe4f68d45572f87938034b
sacha8 <62705893+sacha8@users.noreply.github.com>
2020-05-03 16:52:37 +0200

    EndJam
    On a réussi

---
+++ Assets/sacha/Scripts/Fin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Fin : MonoBehaviour
+{
+    public string fin;
+    public string Menu;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(fin);
+    }
+
+    public void Fini()
+    {
+        SceneManager.LoadScene(Menu);
+    }
+}
---
+++ Assets/sacha/Scripts/OpenDoorBoit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorBoit : MonoBehaviour
+{
+    public GameObject OBJhache;
+    public CraftItem Hache;
+    public GameObject Spawner;
+
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (Hache.iHaveHache == true)
+        {
+            Spawner.transform.Translate(76, 15, 0);
+            Destroy(gameObject);
+            Destroy(OBJhache);
+        }
+    }
+}
