241a8777b3529e1f87b2bb614e36fe212d65e96f
sacha8 <62705893+sacha8@users.noreply.github.com>
2020-05-03 11:20:29 +0200

    Door

---
+++ Assets/sacha/Scripts/GiveItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GiveItem : MonoBehaviour
+{
+    public Inventory inv;
+    public GameObject hache;
+    public bool iHaveHache = false;
+
+    private void Start()
+    {
+        hache.SetActive(false);
+    }
+
+    public void Case1()
+    {
+        if (inv.ferCount >= 5 && inv.buchesCount >= 5)
+        {
+            inv.ferCount -= 5;
+            inv.ferCountText.text = inv.ferCount.ToString();
+            inv.buchesCount -= 5;
+            inv.buchesCountText.text = inv.buchesCount.ToString();
+
+            hache.SetActive(true);
+            iHaveHache = true;
+        }
+        
+    }
+
+    private void Update()
+    {
+        Case1();
+    }
+
+}
