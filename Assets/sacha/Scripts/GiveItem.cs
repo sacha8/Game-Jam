e8feabdc228dbe5352e4b00ff28b8cbc12df21eb
sacha8 <62705893+sacha8@users.noreply.github.com>
2020-05-03 14:14:38 +0200

    Upload

--- Assets/sacha/Scripts/GiveItem.cs
+++ Assets/sacha/Scripts/GiveItem.cs
@@ -19,42 +19,6 @@ public class GiveItem : MonoBehaviour
         hache.SetActive(false);
     }
 
-<<<<<<< HEAD
-    public void Case1()
-    {
-        if (inv.ferCount >= 2.5 && inv.buchesCount >= 2.5 && iHaveHache == false)
-        {
-            iHaveHache = true;
-            hache.SetActive(true);
-            inv.ferCount -= 5;
-            inv.ferCountText.text = inv.ferCount.ToString();
-            inv.buchesCount -= 5;
-            inv.buchesCountText.text = inv.buchesCount.ToString();
-
-            
-            
-        }
-        
-    }
-=======
-//    public void Case1()
-  //  {
-    //    if (inv.ferCount >= 5 && inv.buchesCount >= 5 && iHaveHache == false)
-      //      {
-        //        inv.ferCount -= 5;
-          //      inv.ferCountText.text = inv.ferCount.ToString();
-            //    inv.buchesCount -= 5;
-              //  inv.buchesCountText.text = inv.buchesCount.ToString();
-                //hache.SetActive(true);
-                //iHaveHache = true;
-            //}
-    //}
->>>>>>> e3c96c8a04e1a18cd4ca91bd2d7c95b0cddfeade
-
-    private void Update()
-    {
-        //Case1();
-    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
