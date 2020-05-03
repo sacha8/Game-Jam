7895c5a844a11a24865c5859883049b42c160ffc
KRIMNOUILLE <64409680+KRIMNOUILLE@users.noreply.github.com>
2020-05-03 14:12:26 +0200

    update

--- Assets/sacha/Scripts/GiveItem.cs
+++ Assets/sacha/Scripts/GiveItem.cs
@@ -9,20 +9,19 @@ public class GiveItem : MonoBehaviour
     public Inventory inv;
     public GameObject hache;
     [Header("OpenDoor")]
-    public bool iHaveHache;
+    public bool iHaveHache = false;
     public GameObject PorteEnBoit;
+    public Ennemi AIdestroy;
     public GameObject Spawner;
 
     private void Start()
     {
-        iHaveHache = false;
         hache.SetActive(false);
     }
 
-<<<<<<< HEAD
     public void Case1()
     {
-        if (inv.ferCount >= 2.5 && inv.buchesCount >= 2.5 && iHaveHache == false)
+        if (inv.ferCount >= 5 && inv.buchesCount >= 5 && iHaveHache == false)
         {
             iHaveHache = true;
             hache.SetActive(true);
@@ -30,30 +29,13 @@ public class GiveItem : MonoBehaviour
             inv.ferCountText.text = inv.ferCount.ToString();
             inv.buchesCount -= 5;
             inv.buchesCountText.text = inv.buchesCount.ToString();
-
-            
-            
         }
         
     }
-=======
-//    public void Case1()
-  //  {
-    //    if (inv.ferCount >= 5 && inv.buchesCount >= 5 && iHaveHache == false)
-      //      {
-        //        inv.ferCount -= 5;
-          //      inv.ferCountText.text = inv.ferCount.ToString();
-            //    inv.buchesCount -= 5;
-              //  inv.buchesCountText.text = inv.buchesCount.ToString();
-                //hache.SetActive(true);
-                //iHaveHache = true;
-            //}
-    //}
->>>>>>> e3c96c8a04e1a18cd4ca91bd2d7c95b0cddfeade
 
     private void Update()
     {
-        //Case1();
+        Case1();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -63,20 +45,6 @@ public class GiveItem : MonoBehaviour
             Destroy(PorteEnBoit);
             Destroy(hache);
             Spawner.transform.Translate(76, 15, 0);
-        }
-    }
-
-
-    public void craftHache()
-    {
-        if (inv.ferCount >= 5 && inv.buchesCount >= 5 && iHaveHache == false)
-        {
-            inv.ferCount -= 5;
-            inv.ferCountText.text = inv.ferCount.ToString();
-            inv.buchesCount -= 5;
-            inv.buchesCountText.text = inv.buchesCount.ToString();
-            hache.SetActive(true);
-            iHaveHache = true;
         }
     }
 
